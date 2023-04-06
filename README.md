d9a7760c1b8fae7212c3ae5cd4787df4952bb206
Clearsky <ernie60717@gmail.com>
2023-04-06 17:10:41 +0800

    add

--- README.md
+++ README.md
@@ -3,21 +3,47 @@
 用python實作一個簡單的加密貨幣，可以了解區塊鏈的運作流程。
 
 
-
 # How to use
 
-- /mining
-    - 挖出一個區塊
-- /get_chain
-    - 得到區塊鏈上所有區塊
-- /valid
-    - 驗證區塊鏈
+- /mine [GET]
+    - 打包一個區塊
+- /transactions/new [POST]
+    - 建立新交易
+        - sender
+        - recipient
+        - amount
+- /get_chain    [GET]
+    - 查看區塊鏈
+- /nodes/register   [POST]
+    - 註冊一個新的節點
+- /nodes/resolve [GET]
+    - 如果找到一個有效的鏈，其長度大於主鏈，則進行替換
 
 e.g.
 
-> 127.0.0.1:5000/mining 
+> 127.0.0.1:5000/mine
+
+
+# demo流程
+
+- 用一台電腦開啟程式,port 預設 `5003`，當成主鏈
+    - `python main.py`
+- 在另一台電腦或是再開一個終端機，port 換成另一個，當成測試鏈
+    - `python main.py --port 5004`
+- 先註冊新節點
+    - ![](2023-04-06-11-53-06.png)
+- 在`5004`上先做交易
+    - ![](2023-04-06-11-54-46.png)
+- `5004`打包區塊
+    - ![](2023-04-06-11-56-40.png)
+- `5004`查看當前鏈上的區塊，如果有打包多次，可以看到有大於一個的區塊
+    - `http://127.0.0.1:5004/get_chain`
+- `5003`查看主鏈上的區塊，只有一個創世區塊
+    - `http://127.0.0.1:5003/get_chain`
+- 把`5004`上的鏈合併到主鏈(`5003`)上
+    - 
 
 # Ref
-    - https://www.geeksforgeeks.org/create-simple-blockchain-using-python/
+    - https://medium.com/@vanflymen/learn-blockchains-by-building-one-117428612f46
     
 
--- main.py
+++ main.py
@@ -1,44 +1,108 @@
-from flask import Flask, jsonify
-
+from argparse import ArgumentParser
 from blockchain import *
 
-
+# flask app
 app = Flask(__name__)
 
-# 建立區塊鏈
+# Generate a globally unique address for this node
+node_identifier = str(uuid4()).replace('-', '')
+
+# Instantiate the Blockchain
 blockchain = Blockchain()
 
-# 挖一個區塊
-@app.route('/mining', methods=['GET'])
-def mine_block():
-    previous_block = blockchain.print_previous_block()
-    previous_proof = previous_block['proof']
-    proof = blockchain.proof_of_work(previous_proof)
-    previous_hash = blockchain.hash(previous_block)
-    block = blockchain.create_block(proof, previous_hash)
-    response = {'message': 'A block is MINED',
-                'index': block['index'],
-                'timestamp': block['timestamp'],
-                'proof': block['proof'],
-                'previous_hash': block['previous_hash']}
+# api
+@app.route('/mine', methods=['GET'])
+def mine():
+    # We run the proof of work algorithm to get the next proof...
+    last_block = blockchain.last_block
+    proof = blockchain.proof_of_work(last_block)
+
+    # We must receive a reward for finding the proof.
+    # The sender is "0" to signify that this node has mined a new coin.
+    blockchain.new_transaction(
+        sender="0",
+        recipient=node_identifier,
+        amount=1,
+    )
+
+    # Forge the new Block by adding it to the chain
+    previous_hash = blockchain.hash(last_block)
+    block = blockchain.new_block(proof, previous_hash)
+
+    response = {
+        'message': "New Block Forged",
+        'index': block['index'],
+        'transactions': block['transactions'],
+        'proof': block['proof'],
+        'previous_hash': block['previous_hash'],
+    }
     return jsonify(response), 200
 
-# 得到區塊鏈上所有區塊
+
+@app.route('/transactions/new', methods=['POST'])
+def new_transaction():
+    values = request.get_json()
+
+    # Check that the required fields are in the POST'ed data
+    required = ['sender', 'recipient', 'amount']
+    if not all(k in values for k in required):
+        return 'Missing values', 400
+
+    # Create a new Transaction
+    index = blockchain.new_transaction(values['sender'], values['recipient'], values['amount'])
+
+    response = {'message': f'Transaction will be added to Block {index}'}
+    return jsonify(response), 201
+
+
 @app.route('/get_chain', methods=['GET'])
-def display_chain():
-    response = {'chain': blockchain.chain,
-                'length': len(blockchain.chain)}
+def full_chain():
+    response = {
+        'chain': blockchain.chain,
+        'length': len(blockchain.chain),
+    }
     return jsonify(response), 200
 
-# 驗證區塊鏈
-@app.route('/valid', methods=['GET'])
-def valid():
-    valid = blockchain.chain_valid(blockchain.chain)
-    if valid:
-        response = {'message': 'The Blockchain is valid.'}
+
+@app.route('/nodes/register', methods=['POST'])
+def register_nodes():
+    values = request.get_json()
+    nodes = values.get('nodes')
+    if nodes is None:
+        return "Error: Please supply a valid list of nodes", 400
+
+    for node in nodes:
+        blockchain.register_node(node)
+
+    response = {
+        'message': 'New nodes have been added',
+        'total_nodes': list(blockchain.nodes),
+    }
+    return jsonify(response), 201
+
+
+@app.route('/nodes/resolve', methods=['GET'])
+def consensus():
+    replaced = blockchain.resolve_conflicts()
+
+    if replaced:
+        response = {
+            'message': 'Our chain was replaced',
+            'new_chain': blockchain.chain
+        }
     else:
-        response = {'message': 'The Blockchain is not valid.'}
+        response = {
+            'message': 'Our chain is authoritative',
+            'chain': blockchain.chain
+        }
+
     return jsonify(response), 200
+
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5002)
+    parser = ArgumentParser()
+    parser.add_argument('-p', '--port', default=5003, type=int, help='port to listen on')
+    args = parser.parse_args()
+    port = args.port
+
+    app.run(host='0.0.0.0', port=port,debug=True)
